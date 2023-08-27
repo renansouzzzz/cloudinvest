f9e633592db0b8cc14d19121cb795203cf12222e
renansouzzzz <renansouza.oliv@outlook.com>
2023-08-27 20:25:29 -0300

    initial commit - fastapi + sqlalchemy

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,6 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 import config.database as db
-import json
 
 app = FastAPI()
 sqlite = db.SQLite
@@ -12,26 +11,23 @@ def __init__(self):
     sqlite.__init__(sqlite)
     sqlite.create_tables(sqlite)
     sqlite.insert(sqlite)
+    self.select = sqlite.selectUsers(sqlite)
 
-class Usuario(BaseModel):
+class User(BaseModel):
     name: str
     user: str
     
-@app.get("/select")
-async def read_root():
-    select = await sqlite.selectUsers(sqlite)
-    list = []
+@app.get("/")
+async def getUsers():
+    select = await sqlite.selectUsers()
     
-    for user in select:
-        list.append(user)
-        
-    return  list
+    return select
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-
 if __name__ == "__main__":
     __init__(sqlite)
+    
