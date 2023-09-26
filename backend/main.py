042af59028bea9f5bc0f2dc5d1c5ba763262fdb9
renansouzzzz <renansouza.oliv@outlook.com>
2023-09-25 22:17:09 -0300

    feat: sqlalchemy, create_user, get_users and models

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,28 @@
-from msilib import schema
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from fastapi.responses import JSONResponse
+
 
 from .config.database import SessionLocal, engine
 from sqlalchemy.orm import Session
 
 from . import models
 
-models.Base.metadata.create_all(bind=engine)
-
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+        
+    finally:
+        db.close()
+    
 app = FastAPI()
 
 @app.get("/users")
-def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
+def get_user(db: Session = Depends(get_db)):
+    return JSONResponse(content=db.query(models.User).all()) 
 
 @app.post("/users/create")
-def create_user(db: Session, user: schema.UserCreate):
+def create_user(db: Session, user: models.User.email):
     hash_password = user.password + "notreallyhashed"
     db_user = models.User(email=user.email, hash_password=hash_password)
     db.add(db_user)
