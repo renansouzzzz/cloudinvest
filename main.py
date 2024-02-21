8356299ecb23d000df2fe37758312f41a2a74234
renansouzzzz <renansouza.oliv@outlook.com>
2024-02-20 21:27:39 -0300

    somente um test

--- main.py
+++ main.py
@@ -34,6 +34,11 @@ app.add_middleware(
 
 Base.metadata.create_all(engine)
 
+
+@app.get("/")
+async def read_root():
+    return {"message": "teste!"}
+
 @app.get("/users", tags=['User'])
 def get_all_user():
         return user_repository.getAll()
