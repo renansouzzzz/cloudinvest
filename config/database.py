1e714fcb22939ce82fea4b34dfe0ca2dde5b65c3
renansouzzzz <renansouza.oliv@outlook.com>
2024-03-13 20:31:13 -0300

    databs

--- config/database.py
+++ config/database.py
@@ -4,12 +4,12 @@ from sqlalchemy.orm import sessionmaker
 import logging
 from decouple import config
 
-# DATABASE_URL = config('DATABASE_URL')
+DATABASE_URL = config('DATABASE_URL')
 
 logging.basicConfig()
 logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
-engine = create_engine('mysql://root:B1fADEdb5GaE1HFC2dC54fG3B4-AdfGb@monorail.proxy.rlwy.net:35761/railway')
+engine = create_engine(DATABASE_URL)
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
