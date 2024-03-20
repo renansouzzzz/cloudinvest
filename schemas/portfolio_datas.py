80b56d131c99483e4fa9132a43cfab5d2196734e
renansouzzzz <renansouza.oliv@outlook.com>
2024-03-19 22:29:04 -0300

    refact: delete portfolio to front

--- schemas/portfolio_datas.py
+++ schemas/portfolio_datas.py
@@ -8,7 +8,6 @@ class PortfolioDatasSchema(Base):
     __tablename__ = 'port_datas'
 
     id = Column(Integer, primary_key=True, index=True, nullable=False)
-    id_portfolio = Column(Integer, ForeignKey('portfolio.id', onupdate='CASCADE'), nullable=False, unique=True)
     name = Column(String(100), nullable=False)
     tag: TagDatasPortfolio = Column(Enum(TagDatasPortfolio))
     installment = Column(Integer, nullable=False)
