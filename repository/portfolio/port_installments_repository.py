8d8db396f1b3edf4b9d6e6d111d4f89cddfa6f00
renansouzzzz <renansouza.oliv@outlook.com>
2024-06-24 09:40:42 -0300

    feat: exception try catch on port installments

--- repository/portfolio/port_installments_repository.py
+++ repository/portfolio/port_installments_repository.py
@@ -24,55 +24,59 @@ def getAll():
 
 def getByDate(idUser: int, month: str, year: int):
     with Session(engine) as session:
-        monthStr = ParseToTypes.parseMonthToStr(month)
+        try:
+            monthStr = ParseToTypes.parseMonthToStr(month)
 
-        start_date = datetime(year, int(monthStr[0]), 1, 0, 0, 0, 0)
-        end_date = datetime(year, int(monthStr[0]), int(monthStr[1]), 23, 59, 59, 9999)
+            start_date = datetime(year, int(monthStr[0]), 1, 0, 0, 0, 0)
+            end_date = datetime(year, int(monthStr[0]), int(monthStr[1]), 23, 59, 59, 9999)
 
-        data = session.query(PortfolioDatasInstallmentsMapped, PortfolioDatasMapped). \
-            join(PortfolioDatasMapped,
-                 and_(PortfolioDatasMapped.id == PortfolioDatasInstallmentsMapped.id_port_datas,
-                      # PortfolioDatasInstallmentsMapped.is_paid == False,
-                      PortfolioDatasMapped.id_user == idUser)). \
-            filter(
-            or_(
-                and_(
-                    PortfolioDatasMapped.is_recurring == True,
-                    extract('year', PortfolioDatasMapped.created_at) <= year,
-                    extract('month', PortfolioDatasMapped.created_at) <= int(monthStr[0])
+            data = session.query(PortfolioDatasInstallmentsMapped, PortfolioDatasMapped). \
+                join(PortfolioDatasMapped,
+                     and_(PortfolioDatasMapped.id == PortfolioDatasInstallmentsMapped.id_port_datas,
+                          PortfolioDatasMapped.id_user == idUser)). \
+                filter(
+                or_(
+                    and_(
+                        PortfolioDatasMapped.is_recurring == True,
+                        extract('year', PortfolioDatasMapped.created_at) <= year,
+                        extract('month', PortfolioDatasMapped.created_at) <= int(monthStr[0])
+                    ),
+                    PortfolioDatasInstallmentsMapped.expiration_date.between(start_date, end_date)
                 ),
-                PortfolioDatasInstallmentsMapped.expiration_date.between(start_date, end_date)
-            ),
-            PortfolioDatasInstallmentsMapped.id_user == idUser
-        ). \
-            all()
+                PortfolioDatasInstallmentsMapped.id_user == idUser
+            ). \
+                all()
 
-        if data is None:
-            raise ValueError('Nenhum dado foi encontrado!')
+            if data is None:
+                raise ValueError('Nenhum dado foi encontrado!')
 
-        unified_list = []
+            unified_list = []
 
-        for port_installment, port_data in data:
-            unified_datas = UnifiedAllPortfolioData(
-                id=port_installment.id,
-                idPortData=port_data.id,
-                name=port_data.name,
-                installment=port_data.installment,
-                current_installment=port_installment.current_installment,
-                expiration_date=port_installment.expiration_date,
-                created_at=port_data.created_at,
-                idUser=port_data.id_user,
-                expiration_day=port_data.expiration_day,
-                value_installment=port_installment.value_installment,
-                value=port_data.value,
-                tag=port_data.tag,
-                is_recurring=port_installment.is_recurring,
-                is_paid=port_installment.is_paid
-            )
-            unified_list.append(unified_datas)
+            for port_installment, port_data in data:
+                unified_datas = UnifiedAllPortfolioData(
+                    id=port_installment.id,
+                    idPortData=port_data.id,
+                    name=port_data.name,
+                    installment=port_data.installment,
+                    current_installment=port_installment.current_installment,
+                    expiration_date=port_installment.expiration_date,
+                    created_at=port_data.created_at,
+                    idUser=port_data.id_user,
+                    expiration_day=port_data.expiration_day,
+                    value_installment=port_installment.value_installment,
+                    value=port_data.value,
+                    tag=port_data.tag,
+                    is_recurring=port_installment.is_recurring,
+                    is_paid=port_installment.is_paid
+                )
+                unified_list.append(unified_datas)
+
+        except IntegrityError as e:
+            session.rollback()
+            raise HTTPException(status_code=400, detail=f'Error on database: {e}')
 
         return unified_list
-
+    
 
 def invoicePaid(idInstallment: int):
     with Session(engine) as session:
